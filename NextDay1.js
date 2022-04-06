db0335408548a3e50230a1085dbbb52537c62395
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-06 13:54:47 +0300

    Add problem solution

---
+++ NextDay1.js
@@ -0,0 +1,9 @@
+function nextDay1(year, month, day) {
+
+    let nextDay = new Date(year, month - 1, day + 1);
+    let newYear = nextDay.getFullYear();
+    let newMonth = nextDay.getMonth() + 1;
+    let newDate = nextDay.getDate();
+    console.log(`${newYear}-${newMonth}-${newDate}`);
+}
+nextDay1(2020, 2, 29);
